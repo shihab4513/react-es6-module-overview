69253bf91586356ca846f4e265d2f80de5259da1
shihab4513 <shihab4513@gmail.com>
2024-07-11 08:33:27 +0600

    here we have learn how to retreave data from json file using fetch, and how to retrive data from github reposatory json file using fetch.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,49 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Watch from './Components/Watch/Watch'
+import { useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  // smartwatches.js
+  //  const watches = [
+  //   { id: 1, name: 'Apple Watch Series 7', price: 399 },
+  //   { id: 2, name: 'Samsung Galaxy Watch 5', price: 279 },
+  //   { id: 3, name: 'Garmin Fenix 6', price: 549 },
+  //   { id: 4, name: 'Fitbit Sense', price: 299 },
+  //   { id: 5, name: 'Amazfit GTR 3', price: 179 },
+  //   { id: 6, name: 'Huawei Watch GT 3', price: 229 },
+  //   { id: 7, name: 'Fossil Gen 6', price: 299 },
+  //   { id: 8, name: 'TicWatch Pro 3', price: 299 },
+  //   { id: 9, name: 'Withings Steel HR', price: 199 },
+  //   { id: 10, name: 'Suunto 7', price: 399 }
+  // ];
+
+  const [watches, setWatches] = useState([]);
+  // useEffect(()=>{
+  //   fetch('watches.json')
+  //   .then(res=>res.json())
+  //   .then(data=>setWatches(data))
+  // },[])
+  /*
+  In above useEffect code we fetched data form ./public/watches.json local folder then showed in browser
+  In below useEffect we stored json data in a github repository then copied the raw link of the folder and used it to fetch data. Its not recommended!.
+  */
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/shihab4513/watches-json-data/main/watches.json')
+      .then(res => res.json())
+      .then(data => setWatches(data))
+  }, [])
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {
+        watches.map(watch => <Watch key={watch.id} watch={watch}></Watch>)
+      }
+
     </>
   )
 }
--- src/Components/Watch/Watch.jsx
+++ src/Components/Watch/Watch.jsx
@@ -1,8 +1,11 @@
 
 
-const Watch = () => {
+const Watch = ({watch}) => {
+    const {name,price}=watch;
     return (
         <div>
+            <h1>Name : {name} </h1>
+            <h3>Price : {price} </h3>
             
         </div>
     );
